89600354a893479fb4dbbcd786270c029f541452
VkumarStack <vksogi@gmail.com>
2022-10-07 17:55:33 -0700

    Complete project

--- script.js
+++ script.js
@@ -15,11 +15,52 @@ Book.prototype.toggleRead = function() {
 
 const btn = document.querySelectorAll("form button");
 btn.forEach((button) => {
-    button.addEventListener('submit', function click(event) {
-        let elm = document.createElement("p");
-elm.textContent = "PLACEHOLDER";
+    button.addEventListener('click', function click(event) {
+        let inputs = document.querySelectorAll('input');
+        let form = document.querySelector('form');
+        let info = {};
+        let invalid = false;
+        inputs.forEach((input) => {
+            if (input.matches(':invalid')) {
+                invalid = true;
+                return;
+            }
+            let name = input.getAttribute('name');
+            if (name === 'read')
+                info['read'] = input.checked;
+            else
+                info[input.getAttribute('name')] = input.value;
+        })
+        if (!invalid)
+        {
+            myLibrary.push(new Book(info['author'], info['title'], info['pages'], info['read']));
+            let card = document.createElement('div');
+            card.classList.add('card');
+            card.setAttribute("data-index", myLibrary.length - 1);
+            
+            let author = document.createElement('p');
+            author.innerText = myLibrary[myLibrary.length-1].author;
 
-document.querySelector(".library").appendChild(elm);
+            let title = document.createElement('h1');
+            title.innerText = myLibrary[myLibrary.length-1].title;
+
+            let pages = document.createElement('p');
+            pages.innerText = myLibrary[myLibrary.length-1].pages + " Pages";
+
+            if (info['read'])
+                card.classList.add('read');
+            
+            card.appendChild(author);
+            card.appendChild(title);
+            card.appendChild(pages);
+
+            document.querySelector('.library').appendChild(card);
+            card.addEventListener('click', () => {
+                card.classList.toggle('read');
+                myLibrary[Number(card.getAttribute('data-index'))].toggleRead();
+            })
+        }
+        form.reset();
     });
 });
 
